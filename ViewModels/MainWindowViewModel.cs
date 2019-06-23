e6a4117ce23fd1075b2fd8fa4ef1527feca8d412
Себастьян Перейро <m4r4d3r@hotmail.com>
2019-06-23 20:38:05 +0300

    Add forms

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,6 @@ namespace MonPingV4.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Hello World!";
+        //public string Greeting => "Hello World!";
     }
 }
