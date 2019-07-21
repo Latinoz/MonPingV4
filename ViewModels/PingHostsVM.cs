846d80582ca71ddb758538e014144e4b4e6cf3e0
Себастьян Перейро <m4r4d3r@hotmail.com>
2019-07-21 13:42:30 +0300

    Change 21.07.2019 - answer

--- ViewModels/PingHostsVM.cs
+++ ViewModels/PingHostsVM.cs
@@ -1,43 +1,85 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using MonPingV4.Models;
 
 namespace MonPingV4.ViewModels
 {
-    public class PingHostsVM : ViewModelBase
+    public class PingHostsVM : ViewModelBase, INotifyPropertyChanged
     {
-              
+
         //public PingHostsVM(IEnumerable<HostsItem> items)
         //{
-           //Items = new ObservableCollection<HostsItem>(items);
+        //Items = new ObservableCollection<HostsItem>(items);
 
-           //Items.Add()
+        //Items.Add()
         //}
 
         //public ObservableCollection<HostsItem> Items { get; }
 
-        string Iphost;
+        public TextOutAnswer answer;
 
-        public string iphost
+        string iphost = "127.0.0.1";
+
+        public string Iphost
         {
             get
             {
                 
-                return Iphost;
+                return iphost;
             }
 
             set
             {
-                Iphost = value;
+                iphost = value;
             }
         }
 
-        public void OnClickCommand()
+        string outputAnswer;
+
+        public string OutputAnswer
         {
-            //PingClass obj = new PingClass();
-            //obj.DoPingThreadAsync();
+            get
+            {
+
+                return outputAnswer;
+            }
+
+            //set
+            //{
+            //    outputAnswer = value;
+            //    OnPropertyChanged("OutputAnswer");
+            //}
+        }
+
+        public async void OnClickCommand()
+        {
+            PingClass obj = new PingClass();
+            //var test = obj.DoPingThreadAsync(iphost);
+            answer = await obj.DoPingThreadAsync(iphost);
+            if (answer == TextOutAnswer.Success)
+            {
+                outputAnswer = "ICMP answer received";
+            }
+            else if (answer == TextOutAnswer.Warning)
+            {
+                outputAnswer = "Host is not available!";
+            }
+            else if (answer == TextOutAnswer.Error)
+            {
+               outputAnswer = "Invalid ip address!";
+            }
+            OnPropertyChanged("OutputAnswer");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
         }
 
     }
