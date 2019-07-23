b6cb8daf9ecd3f1120449605202a613e3cc59ed3
Себастьян Перейро <m4r4d3r@hotmail.com>
2019-07-23 14:58:41 +0300

    PingClass

--- Models/PingClass.cs
+++ Models/PingClass.cs
@@ -10,34 +10,31 @@ namespace MonPingV4.Models
     public class PingClass
     {
         //переменная вывода сообщения в окно "вывода"
-        public TextOutAnswer outAnswer;
-
-        private async Task<IPStatus> DoWork(string address)
-        {
-            PingReply reply;
-
-            Ping pingSender = new Ping();
-
-            reply = pingSender.Send(address);
-            Thread.Sleep(1000);
-            return reply.Status;
-        }
+        public TextOutAnswer outAnswer;        
 
         //Метод отправки icmp запросов 
         public async Task<TextOutAnswer> DoPingThreadAsync(string address)
-        { 
+        {
+            Ping pingSender = new Ping();
+
             try
-            {                
+            {
+                PingReply reply;
                 {
-                    {                       
-                        var result = await DoWork(address);
+                    {
+                        var result = await Task.Run(() =>
+                        {
+                            reply = pingSender.Send(address);
+                            Thread.Sleep(1000);
+                            return reply.Status;
+                        });
 
                         if (result == IPStatus.Success)
-                        {                            
+                        {                           
                             outAnswer = TextOutAnswer.Success;
                         }
                         else
-                        {                            
+                        {
                             outAnswer = TextOutAnswer.Warning;
                         }
                         return outAnswer;
